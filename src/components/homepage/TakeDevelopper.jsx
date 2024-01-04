ed57d393243833a639fb7e3de9f787d2f7d01d56
Dev Unbelievable <mbolokobelieve@gmail.com>
2024-01-04 14:00:52 +0100

    end section takeDevelopper all is okaay

--- src/components/homepage/TakeDevelopper.jsx
+++ src/components/homepage/TakeDevelopper.jsx
@@ -11,23 +11,23 @@ const textonboarding = "Une fois votre candidat défini, son intégration commen
 
 const DetailNavigation = ({imgDetail, textDetail}) => {
   return(
-      <div className='flex flex-col gap-6 text-gray-300'>
-        <img src={imgDetail} alt="l'image du besoin" className='rounded-3xl' />
-        <p> {textDetail} </p>
+      <div className='flex flex-col gap-6 text-gray-300 lg:flex-row'>
+        <img src={imgDetail} alt="l'image du besoin" className='rounded-3xl lg:w-[50%] '  />
+        <p className='lg:text-2xl ' > {textDetail} </p>
       </div>
   )
 }
 
 function TakeDevelopper() {
-  const [ title, setTitle ] = useState(0);
+  const [ title, setTitle ] = useState(1);
  
 
   return (
     <section className='  bg-red-500  px-8 py-16 lg:px-16 lg:py-20 '>
-      <h2 className='text-4xl lg:text-5xl  text-gray-300 font-semibold mb-6'>Embauchez le développeur idéal pour votre équipe = {title} </h2>
+      <h2 className='text-4xl lg:text-6xl lg:w-[60%]  text-gray-300 font-semibold mb-16'>Embauchez le développeur idéal pour votre équipe  </h2>
 
-        <div className="flex flex-col gap-16 lg:flex-row ">
-          <div className="flex flex-col gap-6 text-2xl  text-gray-500">
+        <div className="flex flex-col content-start items-start gap-10 lg:flex-row ">
+          <div className="flex flex-col gap-6 text-3xl  text-gray-500 w-[50%]  cursor-pointer">
             <h3 className={"flex flex-row gap-4  " + (title=== 1 ? "text-gray-50" : null)}   onClick={()=> setTitle(1)} >
               <i className= {"  " + ( title === 1 ? "bi bi-circle-fill  " : " bi bi-circle") }></i>Comprendre vos besoins
             </h3>
@@ -37,17 +37,20 @@ function TakeDevelopper() {
             <h3 className={"flex flex-row gap-4  " + (title=== 3 ? "text-gray-50" : null)}  onClick={()=> setTitle(3)}>
               <i className={"  " + ( title === 3 ? "bi bi-circle-fill  ": " bi bi-circle") }></i>Onboarding
             </h3>
-            <div className="hidden lg:flex" >
+            <div className="hidden lg:flex mt-5" >
               <Buttons  />
             </div>
           </div>
 
-          <div className='flex flex-col gap-6'>
+          <div className='flex flex-col items-start gap-6 w-[85%]'>
             
             { title === 1 ? <DetailNavigation imgDetail={imgBesoin} textDetail={textBesoin} /> : null }
             { title === 2 ? <DetailNavigation imgDetail={imgCv} textDetail={textCv} /> : null }
             { title === 3 ? <DetailNavigation imgDetail={imgOnboarding} textDetail={textonboarding} /> : null }
-            <Buttons  className="flex lg:hidden" />
+            <div className="flex lg:hidden mt-16" >
+              <Buttons  className="flex lg:hidden" />
+            </div>
+
 
           </div>
         </div>
